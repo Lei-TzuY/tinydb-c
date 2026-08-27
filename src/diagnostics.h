951d8a17c207a41390a306456755c9e23b42cea5
LeiZ <52287354+Lei-TzuY@users.noreply.github.com>
2026-08-27 09:15:17 +0800

    feat: expose multi-root page ownership diagnostics

--- src/diagnostics.h
+++ src/diagnostics.h
@@ -15,6 +15,14 @@ typedef struct {
     uint32_t height;
 } TinyDBTreeStats;
 
+typedef struct {
+    uint32_t total_pages;
+    uint32_t owned_pages;
+    uint32_t free_pages;
+    uint32_t orphan_pages;
+    uint32_t shared_pages;
+} TinyDBPageOwnershipStats;
+
 const TableSchema* tinydb_find_table_schema(const Table* table, const char* table_name);
 
 bool tinydb_get_tree_stats(Table* table,
@@ -25,5 +33,10 @@ bool tinydb_check_table_tree(Table* table,
                              const char* table_name,
                              char* message,
                              size_t message_size);
+
+bool tinydb_check_page_ownership(Table* table,
+                                 TinyDBPageOwnershipStats* stats,
+                                 char* message,
+                                 size_t message_size);
 
 #endif /* DIAGNOSTICS_H */
