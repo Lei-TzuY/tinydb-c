c8e3617562fff2749e5d8cca291bd06775e10da0
LeiZ <52287354+Lei-TzuY@users.noreply.github.com>
2026-08-28 23:09:58 +0800

    Route bounded internal underflow through redistribution

--- src/record_delete_v2_empty_route.c
+++ src/record_delete_v2_empty_route.c
@@ -3,6 +3,7 @@
 #include "leaf_page_access.h"
 #include "record.h"
 #include "record_delete_v2_empty_leaf.h"
+#include "record_delete_v2_internal_borrow.h"
 #include "record_delete_v2_root_leaf_collapse.h"
 #include "slotted_leaf_v2.h"
 
@@ -89,6 +90,17 @@ bool tinydb_record_delete(Table* table,
                                                     message_size);
         if (collapse == TINYDB_ROOT_LEAF_COLLAPSE_SUCCESS) return true;
         if (collapse == TINYDB_ROOT_LEAF_COLLAPSE_FAILURE) return false;
+
+        TinyDBInternalBorrowResult borrow =
+            tinydb_try_delete_v2_internal_borrow_from_right(table,
+                                                            schema,
+                                                            leaf_page_num,
+                                                            leaf_before,
+                                                            id,
+                                                            message,
+                                                            message_size);
+        if (borrow == TINYDB_INTERNAL_BORROW_SUCCESS) return true;
+        if (borrow == TINYDB_INTERNAL_BORROW_FAILURE) return false;
 
         return tinydb_delete_v2_empty_leaf(table,
                                            schema,
