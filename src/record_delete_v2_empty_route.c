61981fdb5e659322f14bbaed1319fb01ae46aa4f
LeiZ <52287354+Lei-TzuY@users.noreply.github.com>
2026-08-29 02:09:24 +0800

    Route bounded non-root V2 internal merges

--- src/record_delete_v2_empty_route.c
+++ src/record_delete_v2_empty_route.c
@@ -7,6 +7,7 @@
 #include "record_delete_v2_internal_borrow_left.h"
 #include "record_delete_v2_internal_borrow_local.h"
 #include "record_delete_v2_internal_borrow_root.h"
+#include "record_delete_v2_internal_merge_local.h"
 #include "record_delete_v2_internal_merge_root.h"
 #include "record_delete_v2_internal_merge_wide_root.h"
 #include "record_delete_v2_root_leaf_collapse.h"
@@ -139,6 +140,16 @@ bool tinydb_record_delete(Table* table,
 
         TinyDBInternalMergeRootResult merge =
             tinydb_try_delete_v2_internal_merge_wide_root(table,
+                                                          schema,
+                                                          leaf_page_num,
+                                                          leaf_before,
+                                                          id,
+                                                          message,
+                                                          message_size);
+        if (merge == TINYDB_INTERNAL_MERGE_ROOT_SUCCESS) return true;
+        if (merge == TINYDB_INTERNAL_MERGE_ROOT_FAILURE) return false;
+
+        merge = tinydb_try_delete_v2_internal_merge_local(table,
                                                           schema,
                                                           leaf_page_num,
                                                           leaf_before,
