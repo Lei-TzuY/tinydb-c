5244391c198d23d7a6d349f68c317a921042eb3c
LeiZ <52287354+Lei-TzuY@users.noreply.github.com>
2026-08-29 04:35:01 +0800

    Route key-60 recursive V2 delete

--- src/record_delete_v2_empty_route.c
+++ src/record_delete_v2_empty_route.c
@@ -17,6 +17,7 @@
 #include "record_delete_v2_internal_merge_root_height_mirror.h"
 #include "record_delete_v2_internal_merge_root_height_outer_left.h"
 #include "record_delete_v2_internal_merge_root_height_outer_right.h"
+#include "record_delete_v2_internal_merge_root_height_right_inner_right.h"
 #include "record_delete_v2_internal_merge_wide_root.h"
 #include "record_delete_v2_root_leaf_collapse.h"
 #include "slotted_leaf_v2.h"
@@ -212,6 +213,17 @@ bool tinydb_record_delete(Table* table,
         if (merge == TINYDB_INTERNAL_MERGE_ROOT_FAILURE) return false;
 
         merge = tinydb_try_delete_v2_internal_merge_root_height_inner_left(
+            table,
+            schema,
+            leaf_page_num,
+            leaf_before,
+            id,
+            message,
+            message_size);
+        if (merge == TINYDB_INTERNAL_MERGE_ROOT_SUCCESS) return true;
+        if (merge == TINYDB_INTERNAL_MERGE_ROOT_FAILURE) return false;
+
+        merge = tinydb_try_delete_v2_internal_merge_root_height_right_inner_right(
             table,
             schema,
             leaf_page_num,
