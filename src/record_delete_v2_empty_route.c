06a40515ec266ccdc3c5ca6d2b1cf4ebb4bbdff7
LeiZ <52287354+Lei-TzuY@users.noreply.github.com>
2026-08-29 17:09:48 +0800

    storage: route mirrored height-five non-root delete cascade

--- src/record_delete_v2_empty_route.c
+++ src/record_delete_v2_empty_route.c
@@ -8,6 +8,7 @@
 #include "record_delete_v2_internal_borrow_local.h"
 #include "record_delete_v2_internal_borrow_root.h"
 #include "record_delete_v2_internal_merge_borrow_nonroot.h"
+#include "record_delete_v2_internal_merge_borrow_nonroot_left.h"
 #include "record_delete_v2_internal_merge_borrow_root.h"
 #include "record_delete_v2_internal_merge_borrow_root_left.h"
 #include "record_delete_v2_internal_merge_local.h"
@@ -157,6 +158,17 @@ bool tinydb_record_delete(Table* table,
                                                                id,
                                                                message,
                                                                message_size);
+        if (merge == TINYDB_INTERNAL_MERGE_ROOT_SUCCESS) return true;
+        if (merge == TINYDB_INTERNAL_MERGE_ROOT_FAILURE) return false;
+
+        merge = tinydb_try_delete_v2_internal_merge_borrow_nonroot_from_left(
+            table,
+            schema,
+            leaf_page_num,
+            leaf_before,
+            id,
+            message,
+            message_size);
         if (merge == TINYDB_INTERNAL_MERGE_ROOT_SUCCESS) return true;
         if (merge == TINYDB_INTERNAL_MERGE_ROOT_FAILURE) return false;
 
